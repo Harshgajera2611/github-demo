9ec19186390967178fef14fac7613c90a82367dc
Harsh <harshgajera2611@gmail.com>
2022-07-27 11:23:59 +0530

    Initial Commit

---
+++ github demo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  github demo
+//
+//  Created by Harsh Gajera on 27/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let label = UILabel()
+        label.text = "hello"
+        label.backgroundColor = .green
+        label.textAlignment = .center
+        label.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        view.addSubview(label)
+    }
+
+
+}
+
